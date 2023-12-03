02934a81ca100899b53f927986e47607350542ce
Michaël Hoste <michael.hoste@gmail.com>
2023-12-03 10:01:38 +0100

    day 3 - 1

---
+++ day-3/script-1.rb
@@ -0,0 +1,52 @@
+def extract_subline(lines, line_index, col_index, digits_number)
+  col_size        = lines.first.size
+  start_col_index = [col_index - 1, 0].max
+  end_col_index   = [col_index + digits_number, col_size].min
+
+  if line_index >= 0 && line_index < lines.size
+    lines[line_index][start_col_index..end_col_index]
+  else
+    ""
+  end
+end
+
+# Create bounding box around number, and check if special char
+def admissible?(lines, line_index, col_index, digits_number)
+  col_size = lines.first.size
+
+  box_string = ""
+  box_string << extract_subline(lines, line_index - 1, col_index, digits_number)
+  box_string << extract_subline(lines, line_index,     col_index, digits_number)
+  box_string << extract_subline(lines, line_index + 1, col_index, digits_number)
+
+
+  box_string.gsub('.', '').match? /\D/
+end
+
+answer = 0
+
+lines = File.read('./engine.txt').split("\n")
+
+lines.each_with_index do |line, line_index|
+  line     = line.dup
+  col_index = 0
+
+  while line.size > 0
+    if line.to_i > 0 # start of a number
+      number        = line.to_i
+      digits_number = number.to_s.size
+
+      if admissible?(lines, line_index, col_index, digits_number)
+        answer = answer + number
+      end
+
+      col_index = col_index + digits_number
+      line.slice!(0, digits_number) # remove first chars
+    else # everything else
+      line.slice!(0, 1)
+      col_index = col_index + 1
+    end
+  end
+end
+
+puts answer
