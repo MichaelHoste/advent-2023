0597719fdcec56ab025ac746a33dc56fe6594360
Michaël Hoste <michael.hoste@gmail.com>
2023-12-04 11:48:17 +0100

    Day 4-2 🎉

---
+++ day-4/script-2.rb
@@ -0,0 +1,22 @@
+lines = File.read('./cards.txt').gsub(/Card\s+\d+: /, '').split("\n")
+
+cards = lines.collect do |line|
+  nums, wins = line.split(' | ').collect { |n| n.split(' ').collect(&:to_i) }
+
+  {
+    :num_of_cards     => 1,
+    :matching_numbers => (25 - (wins-nums).size).to_i
+  }
+end
+
+cards.each_with_index do |card, index|
+  card[:num_of_cards].times do
+    card[:matching_numbers].times do |i|
+      j = index+i+1
+      cards[j][:num_of_cards] += 1 if j < cards.size
+    end
+  end
+end
+
+puts cards.sum { |card| card[:num_of_cards] }
+
