de77a8c510ef505b9ec4e3d423fbab7a65529139
Michaël Hoste <michael.hoste@gmail.com>
2023-12-09 10:48:31 +0100

    Day 9-2 🎉 (what a joke :smile: )

---
+++ day-9/script-2.rb
@@ -0,0 +1,20 @@
+def next_number(numbers)
+  differences = numbers.collect.with_index do |number, i|
+    numbers[i+1] - number if i < numbers.size - 1
+  end.compact
+
+  if differences.all? { |num| num == 0 }
+    numbers.last
+  else
+    numbers.last + next_number(differences)
+  end
+end
+
+histories = File.read('./history.txt').split("\n").collect do |history|
+  history.split(' ').collect(&:to_i).reverse
+end
+
+puts (histories.sum do |numbers|
+  next_number(numbers)
+end)
+
