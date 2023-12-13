06956e78c1566777d25e05d6bbba16ec44c13669
Michaël Hoste <michael.hoste@gmail.com>
2023-12-13 18:00:28 +0100

    12-2 🎉

--- day-12/script-2.rb
+++ day-12/script-2.rb
@@ -18,33 +18,8 @@ def possible_positions(line, number)
   positions
 end
 
-def arrangements(spring)
-  if spring[:groups].any?
-    number        = spring[:groups].first
-    start_indexes = possible_positions(spring[:line], number)
-
-    start_indexes.collect do |start_index|
-      start          = (spring[:path].size - spring[:line].size) + start_index
-      path           = spring[:path]
-      updated_path   = path[0...start].to_s + ('#' * number) + path[start+number..-1]
-      remaining_line = path[start+number+1..-1].to_s
-      updated_groups = spring[:groups].drop(1)
-
-      if remaining_line.count('?') + remaining_line.count('#') >= updated_groups.sum #+ [updated_groups.size - 1, 0].max
-        arrangements({
-          :path   => updated_path,
-          :line   => remaining_line, # Create substring with empty space + the remaining )
-          :groups => updated_groups
-        })
-      else
-        []
-      end
-    end.flatten
-  else
-    [spring]
-  end
-end
-
+# This method should not be needed, it's a last-minute fix to not include some invalid paths
+# that should have been rejected before but are not (why!?)
 def path_ok?(path, groups)
   path_groups = []
   increment   = 0
@@ -63,6 +38,38 @@ def path_ok?(path, groups)
   groups == path_groups
 end
 
+def arrangements(spring, hash, initial_groups)
+  if spring[:groups].any?
+    if hash[[spring[:line], spring[:groups]]]
+      return hash[[spring[:line], spring[:groups]]]
+    else
+      number        = spring[:groups].first
+      start_indexes = possible_positions(spring[:line], number)
+
+      total = start_indexes.collect do |start_index|
+        start            = (spring[:path].size - spring[:line].size) + start_index
+        path             = spring[:path]
+        updated_path     = path[0...start].to_s + ('#' * number) + path[start+number..-1]
+        remaining_line   = path[start+number+1..-1].to_s
+        remaining_groups = spring[:groups].drop(1)
+
+        arrangements({
+          :path   => updated_path,
+          :line   => remaining_line,
+          :groups => remaining_groups
+        }, hash, initial_groups)
+      end.sum
+
+      hash[[spring[:line], spring[:groups]]] = total
+    end
+  else
+    if path_ok?(spring[:path], initial_groups) # Should not be needed if the above algos were 100% correct but... who cares? 😁
+      1
+    else
+      0
+    end
+  end
+end
 
 springs = File.read('./springs.txt').split("\n").collect do |line|
   {
@@ -72,27 +79,12 @@ springs = File.read('./springs.txt').split("\n").collect do |line|
   }
 end
 
-
 sizes = springs.collect.with_index do |spring, i|
-  size = arrangements(spring).flatten
-                             .select { |spring| spring[:groups].none? }
-                             .uniq { |spring| spring[:path] }
-                             .select { |spring| path_ok?(spring[:path], springs[i][:groups]) }
-                             .size
-  puts "#{i} - #{size}" if MULTIPY > 1
+  size = arrangements(spring, {}, spring[:groups])
+
+  puts "#{i} - #{size}"
   size
 end
 
 puts sizes.sum
 
-# pp possible_positions('#?#????????.?#', 4)
-# pp possible_positions('####???????.?#.', 1)
-
-
-# pp springs[40]
-# pp arrangements(springs[1])
-
-
-# too low: 709232705819
-#          708115787581
-#          712617224806.0437
