410656257d8714532172e733cb8546117fa3fc99
Michaël Hoste <michael.hoste@gmail.com>
2023-12-17 15:26:10 +0100

    Day 17-1

---
+++ day-17/script-1.rb
@@ -0,0 +1,156 @@
+def keep_relevant_moves(node)
+  last_moves = node[:moves].last(3)
+
+  if last_moves.size == 1
+    relevant_moves = last_moves
+  elsif last_moves.size == 2
+    if last_moves.uniq.size == 1
+      relevant_moves = last_moves
+    else
+      relevant_moves = last_moves.last(1)
+    end
+  elsif last_moves.size == 3
+    if last_moves.uniq.size == 1
+      relevant_moves = last_moves
+    elsif last_moves.last(2).uniq.size == 1
+      relevant_moves = last_moves.last(2)
+    else
+      relevant_moves = last_moves.last(1)
+    end
+  end
+
+  node[:moves] = relevant_moves
+
+  return node
+end
+
+def evaluate_h(block, node)
+  #node[:total] + (block.size-1 - node[:i]) + (block.first.size-1 - node[:j])
+
+  vertical   = block.size-1 - node[:i]
+  horizontal = block.first.size-1 - node[:j]
+
+  vertical + horizontal + vertical/3 + horizontal/3
+end
+
+def create_hash_key(node)
+  new_node = node.except(:eval)
+  hash_key = "#{node[:i]}|#{node[:j]}|#{node[:moves].join}"
+end
+
+block = File.read('./block.txt').split("\n").collect do |line|
+  line.split('').collect(&:to_i)
+end
+
+visited_nodes = {}
+
+stack = [{
+  :i      => 0,
+  :j      => 0,
+  :total  => 0,
+  :moves  => []
+}]
+
+current_weight = 0
+
+while true
+  node = stack.shift
+
+  # Print current weight
+  if node[:total] > current_weight
+    current_weight = node[:total]
+    #puts current_weight
+  end
+
+  # Print result that was found
+  if node[:i] == block.size-1 && node[:j] == block.first.size-1
+    puts node.inspect
+    puts node[:total]
+    puts "stack size: #{stack.size}"
+    break
+  end
+
+  next_nodes = []
+
+  # Go left
+  if node[:moves].last != 'r' # last move cannot be left!
+    i = node[:i]
+    j = node[:j] - 1
+    if j >= 0
+      next_nodes << {
+        :i      => i,
+        :j      => j,
+        :total  => node[:total] + block[i][j],
+        :moves  => node[:moves] + ['l']
+      }
+    end
+  end
+
+  # Go right
+  if node[:moves].last != 'l' # last move cannot be right!
+    i = node[:i]
+    j = node[:j] + 1
+    if j < block.first.size
+      next_nodes << {
+        :i      => i,
+        :j      => j,
+        :total  => node[:total] + block[i][j],
+        :moves  => node[:moves] + ['r']
+      }
+    end
+  end
+
+  # Go up
+  if node[:moves].last != 'd' # last move cannot be down!
+    i = node[:i] - 1
+    j = node[:j]
+    if i >= 0
+      next_nodes << {
+        :i      => i,
+        :j      => j,
+        :total  => node[:total] + block[i][j],
+        :moves  => node[:moves] + ['u']
+      }
+    end
+  end
+
+  # Go down
+  if node[:moves].last != 'u' # last move cannot be up!
+    i = node[:i] + 1
+    j = node[:j]
+    if i < block.size
+      next_nodes << {
+        :i      => i,
+        :j      => j,
+        :total  => node[:total] + block[i][j],
+        :moves  => node[:moves] + ['d']
+      }
+    end
+  end
+
+  # Reject potential nodes with 4 moves in same direction!
+  next_nodes = next_nodes.reject do |next_node|
+    next_node[:moves].size >= 4 && next_node[:moves].last(4).uniq.size == 1
+  end
+
+  # Create evaluation of nodes (distance travelled + lower estimation of distance remaining)
+  next_nodes = next_nodes.each do |next_node|
+    next_node[:eval] = next_node[:total] + evaluate_h(block, next_node)
+  end
+
+  next_nodes.each do |next_node|
+    next_node = keep_relevant_moves(next_node)
+    hash_key  = create_hash_key(next_node)
+
+    if !visited_nodes[hash_key] || next_node[:total] < visited_nodes[hash_key]
+      visited_nodes[hash_key] = next_node[:total]
+
+      new_index = stack.bsearch_index { |node| node[:eval] >= next_node[:eval] } || stack.size
+
+      stack.insert(new_index, next_node)
+    end
+  end
+end
+
+# too high: 872
+# too high: 870
